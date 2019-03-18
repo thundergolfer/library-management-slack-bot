07d0736033d8dfb446a08a2a7f120d69fbfe8964
Jonathon Belotti <jonathon@canva.com>
2019-03-18 20:57:21 +1100

    Setting up basic Jest testing

---
+++ tests/Example.test.ts
@@ -0,0 +1,11 @@
+describe('calculate', function() {
+    it('add', function() {
+        let result = 5 + 2;
+        expect(result).toBe(7);
+    });
+
+    it('substract', function() {
+        let result = 3 - 0;
+        expect(result).toBe(3);
+    });
+});
